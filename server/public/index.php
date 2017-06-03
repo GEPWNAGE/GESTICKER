0bae4290df7629506bc85fcf55d96e9edfd1b102
Koen Klaren <koen.klaren@gmail.com>
2017-06-03 15:31:37 +0200

    Use Slim framework

--- server/public/index.php
+++ server/public/index.php
@@ -1,14 +1,12 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<?php
 
-    <title>GEWIS Stickers</title>
-</head>
-<body>
-    <div id="app"></div>
-    <script src="./bundle.js"></script>
-</body>
-</html>
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+require('../vendor/autoload.php');
+
+$app = new \Slim\App();
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write('Hello, world!');
+});
+$app->run();
