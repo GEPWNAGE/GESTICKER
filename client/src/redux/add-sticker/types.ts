814e13d5bde8893d52164d2492db9ff29f6fe821
Koen Klaren <koen.klaren@gmail.com>
2017-06-07 16:17:54 +0200

    Make payload optional

--- client/src/redux/add-sticker/types.ts
+++ client/src/redux/add-sticker/types.ts
@@ -3,3 +3,5 @@ export const SET_TYPE = 'SET_TYPE';
 export const SET_DATE = 'SET_DATE';
 export const SET_COORDS = 'SET_COORDS';
 export const SET_MAP_CENTER = 'SET_MAP_CENTER';
+
+export const SUBMIT_FORM = 'SUBMIT_FORM';
