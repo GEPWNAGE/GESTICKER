fe1b1c48eace2abaac9e56b7a74b218d82602e58
Koen Klaren <koen.klaren@gmail.com>
2017-06-09 23:10:46 +0200

    Remove dummy sticker generation

--- client/src/redux/map/reducers.ts
+++ client/src/redux/map/reducers.ts
@@ -11,29 +11,6 @@ const initialState: MapState = {
     stickers: [],
 };
 
-// TODO: Remove randomly generated stickers
-initialState.stickers.push({
-    id: 1,
-    type: 'placed',
-    date: new Date(),
-    coords: {
-        lat: 51.4473811,
-        lng: 5.4877141,
-    },
-});
-
-for (let i = 0; i < 100; i++) {
-    initialState.stickers.push({
-        id: i + 2,
-        type: 'placed',
-        date: new Date(),
-        coords: {
-            lat: 51.4473811 + (1 - Math.random() * 2) * 0.05,
-            lng: 5.4877141 + (1 - Math.random() * 2) * 0.1,
-        },
-    });
-}
-
 export default function mapReducer(state: MapState, action: Action): MapState {
     if (state === undefined) {
         return initialState;
